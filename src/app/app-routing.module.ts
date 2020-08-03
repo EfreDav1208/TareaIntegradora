830cf1c7fddad2dd8370e8a661ddbbd8a35563c4
unknown <efredav90@gmail.com>
2020-08-02 20:26:02 -0500

    tarea integradora

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AportesComponent } from './componentes/aportes/aportes.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { CompletoComponent } from './componentes/completo/completo.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'inicio', component: AportesComponent},
+  {path:'login', component: LoginComponent},
+  {path:'cuenta', component: PerfilComponent},
+  {path:'logeado', component: InicioComponent},
+  {path:'completo', component: CompletoComponent},
+  {path:'**', pathMatch:'full', redirectTo:'inicio'}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { AportesComponent } from './componentes/aportes/aportes.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { CompletoComponent } from './componentes/completo/completo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    AportesComponent,
+    RegistroComponent,
+    PerfilComponent,
+    
+    InicioComponent,
+    
+    CompletoComponent
   ],
   imports: [
     BrowserModule,
